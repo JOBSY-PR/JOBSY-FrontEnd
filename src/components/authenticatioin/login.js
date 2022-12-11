9e2bf207ea02986bb593d8638cd4fd0b87919982
Emilio Kariuki <emilio113kariuki@gmail.com>
2022-12-12 00:06:45 +0300

    REFACTOR changed the variant type of the textfield to outlined

--- src/components/authenticatioin/login.js
+++ src/components/authenticatioin/login.js
@@ -15,8 +15,8 @@ const Login = () => {
          '& .MuiTextField-root': { m: 1, width: '40ch',  },
        }}>
          <h1>Log In</h1>
-         <TextField type='email' variant='filled' label='Your email'/>
-         <TextField type='password' variant='filled' label='Your password'/>
+         <TextField type='email' variant='outlined' label='Your email'/>
+         <TextField type='password' variant='outlined' label='Your password'/>
          <Link style={{width:'100%', margin:"auto"}} to='/home'><Button fullWidth variant='contained'>LOG IN</Button></Link>
          <p>Don't have an account? <Link style={{color:'black'}} to='/'>Sign Up</Link></p>
      </FormControl>
